a959d02d3c0a74f8b65e3652b786e2ee1335bb25
Oliver Monson-Haefel <monsonha@usc.edu>
2024-03-20 16:17:21 +0000

    equal-paths commit

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,6 +1,6 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
-
+#include <iostream>
 #endif
 
 #include "equal-paths.h"
@@ -8,11 +8,38 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
-
+//helper function that returns like an array of heights?
+//can't do my first idea bc you can't see parent from the node
+//compare heights on l, f, if same do that for l and f nodes too
+int pathHeight(Node * root) {
+	if(root == NULL) {
+		return -1;
+	}
+	if(root->left != NULL && root->right != NULL) {
+		return max(pathHeight(root->left), pathHeight(root->right)) + 1;
+	}
+	else if(root->left == NULL && root->right == NULL) {
+		return 1;
+	}
+	else if(root->left == NULL) {
+		return pathHeight(root->right) + 1;
+	}
+	else {
+		return pathHeight(root->left) + 1;
+	}
+}
 
 bool equalPaths(Node * root)
 {
     // Add your code below
-
+	if(root == NULL) {
+		return true;
+	}
+	if((pathHeight(root->left) == pathHeight(root->right)) || pathHeight(root->left) == -1 || pathHeight(root->right) == -1) {
+		return equalPaths(root->left) && equalPaths(root->right);
+	}
+	else {
+		return false;
+	}
 }
 
